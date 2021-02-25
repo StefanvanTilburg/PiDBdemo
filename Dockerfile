a32394fde7f35206285684b7197d857de52e8759
StefanvanTilburg <TillyAliasStefan@gmail.com>
2021-02-25 23:34:43 +0100

    Added dockerfiles to run application inside docker containers. It works, needs security recheck

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Resolving following docker run warning on Raspberry Pi 4 (arm v7 32bit):
+# OpenJDK Server VM warning: No monotonic clock was available - timed services may be
+# adversely affected if the time-of-day clock changes
+# https://github.com/AdoptOpenJDK/openjdk-docker/issues/469
+# Underlying issue: https://github.com/tianon/docker-brew-ubuntu-core/issues/196
+# Fix for issue: https://packages.debian.org/sid/armhf/libseccomp2/download
+FROM adoptopenjdk:11-jre-hotspot
+USER root
+RUN groupadd -r docker
+RUN useradd -r -u 1001 -g docker spring
+RUN usermod -aG docker spring
+RUN newgrp docker
+USER spring
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/resources/sql/createDB.sql
+++ src/main/resources/sql/createDB.sql
@@ -10,6 +10,13 @@ CREATE SCHEMA IF NOT EXISTS `dbdemo` DEFAULT CHARACTER SET utf8 ;
 USE `dbdemo` ;
 
 -- Set user for dbdemo
-DROP USER IF EXISTS 'user'@'localhost';
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'userpw';
-GRANT ALL ON dbdemo.* to 'user'@'localhost';
+-- DROP USER IF EXISTS 'user'@'localhost';
+-- CREATE USER 'user'@'localhost' IDENTIFIED BY 'userpw';
+-- GRANT ALL ON dbdemo.* to 'user'@'localhost';
+
+-- This is not secure but for needed for testing purpose
+-- This works for now but setting a user like this is not secure. Has to do with internal ip / docker container
+-- / what does docker see as localhost
+DROP USER IF EXISTS 'user'@'%';
+CREATE USER 'user'@'%' IDENTIFIED BY 'userpw';
+GRANT ALL ON dbdemo.* to 'user'@'%';
